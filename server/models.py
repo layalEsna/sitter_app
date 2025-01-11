174913d488eca152868f84cec56adf3dc1cbc083
Amene <layla_1380@yahoo.com>
2025-01-10 20:59:31 -0600

    Apply hashing logic and use regex for password

--- server/models.py
+++ server/models.py
@@ -4,6 +4,7 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 import  pgeocode
 from datetime import datetime, date
+import re
 
 db = SQLAlchemy()
 bcrypt = Bcrypt() 
@@ -14,3 +15,19 @@ class PetOwner(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String, nullable=False, unique=True)
     _hash_password = db.Column(db.String, nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+    
+    @password.setter
+    def password(self, password):
+        pattern = re.compile(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*]).{8,}$')
+        if not pattern.match(password):
+            raise ValueError('Password must be at least 8 characters long. It must include at least 1 lowercase, 1 uppercase letter, and at least 1 (!@#$%^&*)')
+        self._hash_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self._hash_password, password)
+    
+    
