ffe9ec8b1ee258f53d0fe25493df9659a039e8c1
Amene <layla_1380@yahoo.com>
2025-01-10 21:56:46 -0600

    Add signup route with validation and error handling

---
+++ server/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify,session
+import logging
+
+from flask_migrate import Migrate
+from models import db, bcrypt, PetOwner, PetSitter , Appointment
+from datetime import date
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000"])
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+bcrypt.init_app(app)
+
+migrate = Migrate(app, db)  
+
+
+
+@app.route("/")
+def home():
+    return "Welcome to the Pet Sitter app!"
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+  try:
+    data = request.get_json()
+    user_name = data.get('user_name')
+    password = data.get('password')
+
+    if not all([user_name, password]):
+        return jsonify({'error': 'All fields are required'}), 400
+    if PetOwner.query.filter(PetOwner.user_name == user_name).first():
+        return jsonify({'error': 'Username already exists. Please choose another one.'}), 400
+    
+    new_user = PetOwner(
+        user_name = user_name,
+        password = password
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+
+    session['user_id'] = new_user.id
+    return jsonify({'message': 'Successful signup.'}), 201
+  except Exception as e:
+     logging.error(f'An error occurred during signup: {e}')
+     return jsonify({'error': 'Server or network error.'}), 500
+
+
+
+
